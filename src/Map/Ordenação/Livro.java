d97d94ab6314cba9bea92533bbe7a2b57e293693
Kaiky Pires <kaiky.pires@yahoo.com.br>
2024-06-05 21:29:57 -0300

    comitt2

--- src/Map/Ordenação/Livro.java
+++ src/Map/Ordenação/Livro.java
@@ -47,5 +47,6 @@ class ComparatorPorAutor implements Comparator<Map.Entry<String, Livro>> {
     @Override
     public int compare(Map.Entry<String, Livro> l1, Map.Entry<String, Livro> l2) {
         return l1.getValue().getAutor().compareToIgnoreCase(l2.getValue().getAutor());
+        \\teste
     }
 }
