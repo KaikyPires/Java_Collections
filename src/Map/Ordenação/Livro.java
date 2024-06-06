245a5a4697696d0421d6efa4f726ec719b6a1ad1
Kaiky Pires <kaiky.pires@yahoo.com.br>
2024-06-05 21:16:50 -0300

    commit diff

--- src/Map/Ordenação/Livro.java
+++ src/Map/Ordenação/Livro.java
@@ -3,7 +3,7 @@ package Map.Ordenação;
 import java.util.Comparator;
 import java.util.Map;
 
-public class Livro {
+public class Livro  {
     private String titulo;
     private String autor;
     private double preco;
@@ -47,5 +47,6 @@ class ComparatorPorAutor implements Comparator<Map.Entry<String, Livro>> {
     @Override
     public int compare(Map.Entry<String, Livro> l1, Map.Entry<String, Livro> l2) {
         return l1.getValue().getAutor().compareToIgnoreCase(l2.getValue().getAutor());
+        \\teste
     }
 }
